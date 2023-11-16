f8fc3430304b3fd656f97da8182004107f93120b
sivana <@lumorow>
2023-11-16 18:24:41 +0700

    user-service

---
+++ internal/user/user_service.go
@@ -0,0 +1,22 @@
+package user
+
+import (
+	"context"
+	"time"
+)
+
+type service struct {
+	Repository
+	timeout time.Duration
+}
+
+func NewService(repository Repository) Service {
+	return &service{
+		repository,
+		time.Duration(2) * time.Second,
+	}
+}
+
+func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error) {
+
+}
